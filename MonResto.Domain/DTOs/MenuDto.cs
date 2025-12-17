33cd09a819867c9e16bd4a31ebc5141cf8b008f0
hatembellaaj <hatem.bellaaj@redcad.org>
2025-12-17 10:46:42 +0100

    Fix AutoMapper mapping for MenuDto

--- MonResto.Domain/DTOs/MenuDto.cs
+++ MonResto.Domain/DTOs/MenuDto.cs
@@ -2,5 +2,12 @@ using System.Collections.Generic;
 
 namespace MonResto.Domain.DTOs;
 
-public record MenuDto(int MenuId, string Title, string? Description, IEnumerable<ArticleDto> Articles);
+public record MenuDto
+{
+    public int MenuId { get; init; }
+    public string Title { get; init; } = string.Empty;
+    public string? Description { get; init; }
+    public IEnumerable<ArticleDto> Articles { get; init; } = new List<ArticleDto>();
+}
+
 public record MenuCreateDto(string Title, string? Description, IEnumerable<int> ArticleIds);
